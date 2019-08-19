8c5d75d0772b42555c18d8d0ede30372bfe2a1e7
Javier Moreno <email@javier-moreno.com>
2019-08-19 11:08:38 +0200

    Updated the way docker is installed

--- workshop/initvm.sh
+++ workshop/initvm.sh
@@ -2,10 +2,7 @@
 
 sudo apt update
 sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic test"
-sudo apt update
-sudo apt install docker-ce -y
+curl -sSL https://get.docker.com | sh
 sudo apt install python-pip -y
 pip install azure-iot-edge-runtime-ctl
 sudo apt-get remove unscd -y
